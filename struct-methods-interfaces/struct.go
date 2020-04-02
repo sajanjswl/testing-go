c443d5d993fe194ab7a7ebb9a99eb74e98b28f7c
Sajan Jaiswal <sjnjaiswal@gmail.com>
2020-04-02 13:08:53 +0530

    testing struct
    Signed-off-by: Sajan Jaiswal <sjnjaiswal@gmail.com>

---
+++ struct-methods-interfaces/struct.go
@@ -0,0 +1,33 @@
+package struct_method_interfaces
+
+import (
+	"math"
+)
+
+
+type Rectangle struct {
+
+	Height float64
+	Width float64
+
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func Perimeter(rectangle Rectangle) float64{
+
+	return 2*(rectangle.Height+rectangle.Width)
+}
+
+
+func (rectangle *Rectangle) Area() float64{
+
+	return rectangle.Height*rectangle.Width
+}
+
+
+func (c *Circle)Area()float64{
+    return math.Pi * c.Radius * c.Radius
+}
---
+++ struct-methods-interfaces/struct_test.go
@@ -0,0 +1,41 @@
+package struct_method_interfaces
+
+import (
+	"testing"
+)
+
+func TestPerimeter(t *testing.T) {
+
+	rectangle := Rectangle{4, 5}
+	got := Perimeter(rectangle)
+
+	want := float64(2 * (4 + 5))
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		got := rectangle.Area()
+		want := 72.0
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
+}
